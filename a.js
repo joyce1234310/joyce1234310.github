84ef9b20f6ac53faa45a059d397e476329ffa5fd
joyce1234310 <47174547+joyce1234310@users.noreply.github.com>
2019-01-30 17:44:02 +0800

    Add files via upload

---
+++ a.js
@@ -0,0 +1,14 @@
+var a=number(prompt(What is the number))
+var b=number(prompt(What is the number))
+funtion f(a,b){
+    if (a>b){
+        return("A大於B");
+    }
+    else if(a<b){
+        return("A大於B");
+    }
+    else{
+        return("錯誤")
+    }
+    return
+}
